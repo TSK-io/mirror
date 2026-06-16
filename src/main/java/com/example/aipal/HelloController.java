4c1cfff849f91e82ff32b827632a23336cfb13fa
TSK-io <257826625+TSK-io@users.noreply.github.com>
2026-06-16 09:46:48 +0000

    chore: add comment placeholder in HelloController

--- src/main/java/com/example/aipal/HelloController.java
+++ src/main/java/com/example/aipal/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController{
-
+//qwq
   private final GreetingService greetingService;
 
   public HelloController(GreetingService greetingService){
